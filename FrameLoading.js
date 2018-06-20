343adb3516cc0cbf944851319a967d63a25d94d1
HanYoungsoo <gglife333@gmail.com>
2018-06-20 17:47:54 +0900

    rm unnecessary components from react-native

--- FrameLoading.js
+++ FrameLoading.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { View, Modal, StyleSheet, Image, Text } from "react-native"
+import { View, Modal } from "react-native"
 import { PropTypes } from "prop-types"
 
 export default class FrameLoading extends React.Component {
