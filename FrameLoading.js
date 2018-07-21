7bf70c33c38bc8053b38131681b62d2f99301646
HanYoungsoo <gglife333@gmail.com>
2018-07-22 03:05:40 +0900

    fix a bug & clean code

--- FrameLoading.js
+++ FrameLoading.js
@@ -1,62 +1,71 @@
-import React from "react"
-import { View, Modal } from "react-native"
-import { PropTypes } from "prop-types"
+import React from 'react';
+import { View, Modal } from 'react-native';
+import PropTypes from 'prop-types';
 
 export default class FrameLoading extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      viewOrder: 0
-    }
-    this.increase = 1
+      viewOrder: 0,
+    };
+    this.increase = 1;
+  }
+
+  componentDidMount() {
+    const { viewOrder } = this.state;
+    this._mounted = true;
+    this.setState({ viewOrder });
   }
 
   componentDidUpdate() {
     if (this.props.animating) {
-      const duration = this.props.duration
-      this._rotateView(duration)
+      const { duration } = this.props;
+      this._rotateView(duration);
     }
   }
 
-  _rotateView = duration => {
-    const views = this.props.views
-    const { viewOrder } = this.state
-
-    setTimeout(() => {
-      const viewOrder = this.increase++ % views.length
-      this.setState({ viewOrder })
-    }, duration)
+  componentWillUnmount() {
+    this._mounted = false;
   }
 
+  _rotateView = duration => {
+    const { views } = this.props;
+
+    setTimeout(() => {
+      const viewOrder = (this.increase + 1) % views.length;
+      if (this._mounted) {
+        this.setState({ viewOrder });
+      }
+    }, duration);
+  };
+
   render() {
-    const { viewOrder } = this.state
-    const view = this.props.views[viewOrder]
-    const { loadingContainerStyle } = this.props
+    const { viewOrder } = this.state;
+    const view = this.props.views[viewOrder];
+    const { loadingContainerStyle } = this.props;
 
     return (
       <Modal {...this.props.modalProps} visible={this.props.animating}>
         <View style={loadingContainerStyle}>{view}</View>
       </Modal>
-    )
+    );
   }
 }
 
-FrameLoading.proptypes = {
+FrameLoading.propTypes = {
   animating: PropTypes.bool.isRequired,
-  view: PropTypes.array.isRequired,
-  loadingContainerStyle: PropTypes.object,
+  views: PropTypes.arrayOf(PropTypes.element).isRequired,
+  loadingContainerStyle: PropTypes.shape({}),
   duration: PropTypes.number,
-  modalProps: PropTypes.object
-}
+  modalProps: PropTypes.shape({}),
+};
 
 FrameLoading.defaultProps = {
-  animating: false,
-  views: [],
   loadingContainerStyle: {
-    justifyContent: "center",
-    alignItems: "center",
-    flex: 1
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
   },
   duration: 450,
-  modalProps: {}
-}
+  modalProps: {},
+};
